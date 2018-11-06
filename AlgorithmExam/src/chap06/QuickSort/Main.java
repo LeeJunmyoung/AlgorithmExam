c6f5658945b624dae56cf4481046441db7258911
lee <wnsaud6016@naver.com>
2018-11-06 16:58:39 +0900

    개선된 pivot 추가

--- AlgorithmExam/src/chap06/QuickSort/Main.java
+++ AlgorithmExam/src/chap06/QuickSort/Main.java
@@ -1,16 +1,23 @@
 package chap06.QuickSort;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
-
-		int[] arrs = { 69, 10, 30, 2, 16, 8, 31, 22 ,10,10,10,22,33,16,100,1};
-		QuickSort.quickSort(arrs, 0, arrs.length - 1); 
+		int data[] = { 69, 10, 30, 2, 16, 8, 31, 22 ,10,10,10,22,33,16,100,1,1,-2,50,23,23,23,1,-2};
 		
-		System.out.println("결과");
+		int data2[] = data.clone();
+		
+		
+		System.out.println("- 최 좌측 pivot 정렬 전: "+ Arrays.toString(data));
+		QuickSort.quickSort(data, 0, data.length - 1); 
+		System.out.println("- 최 좌측 pivot 정렬 후: "+ Arrays.toString(data));
 
-		for (int i : arrs) {
-			System.out.print(i + " ");
-		}
+		System.out.println();
+        
+		System.out.println("- 개선된 pivot 정렬 전: "+ Arrays.toString(data2));
+        ImprovedPivotQuickSort.sort(data2, 0, data.length - 1);
+        System.out.println("- 개선된 pivot 정렬 후: "+ Arrays.toString(data2));
 
 	}
 
